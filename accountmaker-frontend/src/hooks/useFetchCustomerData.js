b01cfb86a9c51a9d09a067968c8c2be6c1147163
hamza-zareen <hamza.zareen007@gmail.com>
2023-03-29 19:16:05 +0500

    added fronend support for authentication

--- accountmaker-frontend/src/hooks/useFetchCustomerData.js
+++ accountmaker-frontend/src/hooks/useFetchCustomerData.js
@@ -1,15 +1,20 @@
 import { useState, useEffect } from 'react';
 
-export const useFetchCustomerData = (url) => {
+export const useFetchCustomerData = (url, auth) => {
   const [data, setData] = useState();
   const [isFetching, setIsFetching] = useState(true);
   const [error, setError] = useState(null);
 
   useEffect(() => {
     const abortCont = new AbortController();
-
     setTimeout(() => {
-      fetch(url, { signal: abortCont.signal })
+      fetch(url, 
+        {
+          headers: { "Content-Type": "application/json",
+          "Access-Control-Allow-Origin": "http://localhost:8080",
+          'Access-Control-Allow-Credentials': 'true',
+            Authorization: `Bearer ${auth}`
+          }, signal: abortCont.signal })
         .then((res) => {
           if (!res.ok) {
             throw Error('could not fetch')
